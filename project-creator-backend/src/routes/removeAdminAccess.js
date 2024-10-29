7aca0417c5ec02b1cb1d0731922307305cf0ff2e
Vaiditya2207 <vaiditya.t23csai@nst.rishihood.edu.in>
2024-10-29 20:23:59 +0530

    chore: Remove admin access functionality

---
+++ project-creator-backend/src/routes/removeAdminAccess.js
@@ -0,0 +1,50 @@
+import pool from '../db/config.js';
+
+const removeAdminAccess = async (req, res) => {
+    const userId = req.params.userId;
+    if (isNaN(parseInt(userId))) {
+        return res.status(400).json({
+            message: "UserId is not provided",
+            error: "Wrong Type of userId"
+        });
+    }
+    const connection = await pool.getConnection();
+    try {
+        const authKey = req.headers.authorization;
+        if (!authKey) {
+            return res.status(401).json({
+                message: "Unauthorized",
+                error: "No auth key provided"
+            });
+        }
+        if (authKey !== process.env.AUTH_KEY) {
+            return res.status(401).json({
+                message: "Unauthorized",
+                error: "Invalid auth key"
+            });
+        }
+
+        const [findUser] = await connection.query("SELECT * FROM users WHERE id = ?", [userId]);
+        if (findUser.length === 0) {
+            return res.status(404).json({
+                message: "User not found",
+                error: "User not found"
+            });
+        }
+
+        await connection.query("UPDATE users SET admin = FALSE WHERE id = ?", [userId]);
+        res.status(200).json({
+            message: "User is no longer an admin",
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: err
+        });  
+    } finally {
+        connection.release()
+    }
+}
+
+export default removeAdminAccess;
