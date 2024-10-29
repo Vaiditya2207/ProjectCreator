064ed756290d160998cb2d7e5d8468f42e0e89fa
Vaiditya2207 <vaiditya.t23csai@nst.rishihood.edu.in>
2024-10-29 12:18:08 +0530

    error in port allocation

--- project-creator-backend/src/routes/downloadLatestVersion.js
+++ project-creator-backend/src/routes/downloadLatestVersion.js
@@ -48,11 +48,4 @@ const downloadLatestVersion = async (req, res) => {
     }
 };
 
-const app = express();
-app.get('/download-latest', downloadLatestVersion);
-
-app.listen(8000, () => {
-    console.log('Server running on port 8000');
-});
-
 export default downloadLatestVersion;
